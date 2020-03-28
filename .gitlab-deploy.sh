7e7724b9b8aca082a2f802c9fe5f2d85b82c66b4
umangraval <umangraval2000@gmail.com>
2020-03-28 18:29:07 +0530

    hostkeychecking

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -4,4 +4,4 @@ set -f
 string=$DEPLOY_SERVER
 #Iterate servers for deploy and pull last commit
 echo "Deploy project on server "    
-ssh covid19@${string} "cd api_management && git pull origin master"
+ssh -o StrictHostKeyChecking=no covid19@${string} "cd api_management && git pull origin master"
