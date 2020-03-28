141c585ed8ef52978fc47fcea0f3eeaa0215eb07
umangraval <umangraval2000@gmail.com>
2020-03-28 21:13:48 +0530

    new api

--- flaskapp/hashtag.py
+++ flaskapp/hashtag.py
@@ -1,10 +1,8 @@
-import requests, json
-
-url = "https://api.ritekit.com/v1/search/trending?green=1&corona=1"
-hashtags = []
-response = requests.request("GET", url)
-data = json.loads(response.text)
-for hashtag in data['tags']:
-  hashtags.append("#"+hashtag['tag'])
-hashtags = " ".join(hashtags)
-print(hashtags)
+import requests 
+from bs4 import BeautifulSoup 
+  
+URL = "http://best-hashtags.com/hashtag/indian/"
+r = requests.get(URL) 
+soup = BeautifulSoup(r.content, 'html5lib') 
+hashtgs = soup.find('p1').getText()
+print(hashtgs)
