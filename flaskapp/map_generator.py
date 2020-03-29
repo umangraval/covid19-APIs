29aebd66b76166662cc3f69ce4e2ef2ebdbb6c1d
umangraval <umangraval2000@gmail.com>
2020-03-29 17:15:07 +0000

    filtered hashtags with stats

--- flaskapp/map_generator.py
+++ flaskapp/map_generator.py
@@ -12,25 +12,45 @@ import urllib3
 # def calc_colour(value):
 
 if __name__ == "__main__":
-    # Load the JSON File and Fetch the Data
+
     http = urllib3.PoolManager()
-    r = http.request('GET', 'https://api.rootnet.in/covid19-in/stats/latest')
+    r = http.request('GET', 'https://api.covid19india.org/data.json')
     data =  json.loads(r.data.decode('utf-8'))
-    new = pd.DataFrame.from_dict(data['data']['regional'])
-    print(new.columns)
-    # Including the Ladakh UT stats along with Jammu and Kashmir Region Only and other Mofifications
-    for x in new.columns[1:]:
-        new.loc[new['loc'] == 'Jammu and Kashmir',x] =   int(new.loc[new['loc'] == 'Jammu and Kashmir'][x].values + new.loc[new['loc'] == 'Ladakh'][x].values)
+    newdata = {
+        "success": "true",
+        "data":{
+            "summary":{},
+            "regional":[]
+        }
+    }
+    states = data["statewise"]
+    for state in states:
+        if(state["state"] == "Total"):
+            tt = {}
+            tt["total"] = state["confirmed"]
+            tt["discharged"] = state["recovered"]
+            tt["deaths"] = state["deaths"]
+            newdata["data"]["summary"] = tt
+        else:
+            st = {}
+            st["loc"] = state["state"]
+            st["confirmedCases"] = state["confirmed"]
+            st["discharged"] = state["recovered"]
+            st["deaths"] = state["deaths"]
+            newdata["data"]["regional"].append(st)
+    new = pd.DataFrame.from_dict(newdata['data']['regional'])
+    print('Data Fetched')
+
     new.drop(new[new['loc'] == 'Ladakh'].index, inplace = True)
     new.loc[new['loc'] == 'Telengana', 'loc'] = 'Telangana'
     new.loc[new['loc'] == 'Jammu and Kashmir', 'loc'] = 'Jammu & Kashmir'
     new.reset_index(level=0, inplace=True)
     new.drop('index', axis = 1,inplace = True)
-    new.columns = ['st_nm', 'confirmedCasesIndian', 'confirmedCasesForeign', 'discharged','deaths']
+    new.columns = ['st_nm', 'confirmedCases', 'discharged','deaths']
     states = list(new['st_nm'])
 
 
-    # ## Initialize the Visualization and Read the Template
+    ## Initialize the Visualization and Read the Template
     image = cv2.imread('template2.png')
     image[np.where((image==[255,255,255]).all(axis=2))] = [0,0,0];
     sns.set(style = 'whitegrid', palette = 'pastel', color_codes = True)
@@ -42,6 +62,7 @@ if __name__ == "__main__":
     sdf = gpd.read_file(shp_path)
 
     # Considering only the Mainland India and making modifications in dataframe
+    print('Setting up the Map')
     sdf.loc[sdf['st_nm'] == 'NCT of Delhi','st_nm'] = 'Delhi'
     sdf = sdf.iloc[1:,:]
     sdf.drop([16], inplace = True)
@@ -50,19 +71,15 @@ if __name__ == "__main__":
     ssdf = pd.DataFrame(sdf[sdf['st_nm'].isin(states)])
     ssdf.reset_index(level=0, inplace=True)
     ssdf.drop('index', axis = 1, inplace = True)
-    print(ssdf)
     st_names = list(ssdf.st_nm)
-
 
     # Calculating centroid points
     ssdf['centroid_x'] = 0
     ssdf['centroid_y'] = 0
     ssdf['confirmedCases'] = 0
-    print(ssdf.columns)
-    print(new.columns)
     for i in ssdf.index:
         st = ssdf.loc[i]['st_nm']
-        cc = new.loc[new['st_nm'] == st]['confirmedCasesForeign'].values + new.loc[new['st_nm'] == st]['confirmedCasesIndian'].values
+        cc = new.loc[new['st_nm'] == st]['confirmedCases'].values
         ssdf.loc[i, 'centroid_x'] = ssdf.loc[i,'geometry'].centroid.x
         ssdf.loc[i, 'centroid_y'] = ssdf.loc[i,'geometry'].centroid.y
         ssdf.loc[i, 'confirmedCases'] = cc
@@ -71,15 +88,9 @@ if __name__ == "__main__":
     y_list = list(ssdf['centroid_y'])
     confirmedCases = list(ssdf['confirmedCases'])
 
-    ## Setting the Bomb marker
-    bomb = cv2.imread('')
-
-
-
-
     plt.axis('off')
     for i,st in enumerate(st_names):
-        # print(st)
+
         if st == 'Punjab':
             plt.text(x_list[i]-1.5,y_list[i]-0.2,confirmedCases[i], fontsize=15,fontweight = 'extra bold', color = 'red')
         elif st == "Chandigarh":
@@ -93,8 +104,8 @@ if __name__ == "__main__":
         else:
             plt.text(x_list[i]+0.5,y_list[i]-0.2,confirmedCases[i], fontsize=15,fontweight = 'extra bold', color = 'red')
         plt.scatter(x_list[i],y_list[i],c = 'black', s = 100, marker='o')
+
     mine = cv2.imread('map final 2.png')
-    # mine[np.where((image==[255,255,255]).all(axis=2))] = [0,0,0];
     plt.savefig('map.png')
     map = cv2.imread('map.png')
     map = cv2.resize(map, (image.shape[0], image.shape[1]))
@@ -106,4 +117,5 @@ if __name__ == "__main__":
     langs = ['english','telugu','bengali','tamil','malayalam']
     for lan in langs:
         cv2.imwrite('./Posts/Final_'+lan+'.jpg',final)
+
 
