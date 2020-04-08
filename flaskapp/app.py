ff2726f54437361675e0550b505b8249acd5af35
umangraval <umangraval2000@gmail.com>
2020-04-08 13:48:17 +0530

    USA stats

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -2,6 +2,8 @@ from flask import render_template, Flask, request, json
 from googletrans import Translator
 import requests
 translator = Translator()
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -31,6 +33,35 @@ def getstats():
   retJSON = requests.get('https://api.rootnet.in/covid19-in/stats/latest')
   data = retJSON.json()
   return data
+
+@app.route('/usa')
+def getUSAStats():
+  website_url = requests.get('https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_the_United_States').text
+  soup = BeautifulSoup(website_url,'lxml')
+  My_table = soup.find('table',{'class':'wikitable plainrowheaders sortable'})
+  retJSON = {}
+  data = []
+  links = My_table.find_all('tr')
+
+  for link in links:
+      dataTag = link.find_all('td')
+      headingTag = link.find_all('th')
+      
+      if(len(headingTag) == 2):
+          state = headingTag[1].text.rstrip("\n")
+          if(len(dataTag) == 5):
+              cases = dataTag[0].text.rstrip("\n")
+              deaths = dataTag[1].text.rstrip("\n")
+              recovered = dataTag[2].text.rstrip("\n")
+              region = {
+                  "state": state,
+                  "cases": cases,
+                  "deaths": deaths,
+                  "recovered": recovered
+              }
+              data.append(region)
+  retJSON['data'] = data
+  return retJSON
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',port='8001', debug=True)
