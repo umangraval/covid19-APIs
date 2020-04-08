fc6771786c9ddec80e8adbce735360b553525493
umangraval <umangraval2000@gmail.com>
2020-04-08 14:24:28 +0530

    spain stats

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -63,5 +63,30 @@ def getUSAStats():
   retJSON['data'] = data
   return retJSON
 
+@app.route('/spain')
+def getSPAINStats():
+  website_url = requests.get('https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Spain').text
+  soup = BeautifulSoup(website_url,'lxml')
+  My_table = soup.find('table',{'class':'wikitable sortable'})
+  retJSON = {}
+  data = []
+  links = My_table.find_all('tr')
+  for link in links:
+      dataTag = link.find_all('td')
+      if(len(dataTag) == 6):
+          state = dataTag[0].text.rstrip("\n").replace('(article)','')
+          cases = dataTag[1].text.rstrip("\n")
+          deaths = dataTag[4].text.rstrip("\n")
+          recovered = dataTag[5].text.rstrip("\n")
+          region = {
+              "state": state,
+              "cases": cases,
+              "deaths": deaths,
+              "recovered": recovered
+          }
+          data.append(region)
+  retJSON['data'] = data
+  return retJSON
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0',port='8001', debug=True)
